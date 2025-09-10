205bb21392df9b17a85668798bf371170a5f473e
Ricardo Toledo <64061937+ricardotoledo41@users.noreply.github.com>
2025-09-10 02:02:51 +0000

    adiciona scripts`lint:check`e`lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto pra aprender programacao
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home(){ 
-    return <h1>Eu te amo lucianaaaa!!!!!!! a Sofia e nossa thuthuquinha amo vcs duas de ++++++++++++</h1>
-
+function Home() {
+  return (
+    <h1>
+      Eu te amo lucianaaaa!!!!!!! a Sofia e nossa thuthuquinha amo vcs duas de
+      ++++++++++++
+    </h1>
+  );
 }
-
 
 export default Home;
